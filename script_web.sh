8e8425f0d9440a25a3102db175a80bb864126c65
WiktorBuczko <wiktorusxxd@gmail.com>
2017-02-16 22:28:41 +0100

    Update script_web.sh

--- script_web.sh
+++ script_web.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-URL=<URL of a web page>
-timeb=<time in seconds between checks>
-mail=<your e-mail address>
-title=<title of an e-mail message>
-message=<your message in email>
+URL="<URL of a web page>"
+timeb="<time in seconds between checks>"
+mail="<your e-mail address>"
+title="<title of an e-mail message>"
+message="<your message in email>"
 wynikold=`curl -so /dev/null $URL -w '%{size_download}'`
 wyniknew=`curl -so /dev/null $URL -w '%{size_download}'`
 echo $wynikold
